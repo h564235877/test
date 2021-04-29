2310118c13c01e14eebd0ef376bf812edc1c4905
h564235877 <564235877@qq.com>
2021-04-29 22:56:11 +0800

    首次提交

---
+++ test/src/views/News.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="new">就是想玩一下
+    <div class="a">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "News",
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<style lang="less" scoped>
+  .new {
+    width: 1080px;
+    height: 500;
+    background-color: red;
+  }
+</style>
